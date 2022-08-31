1029555e40986a956ee86c1254c6e79bdadeedbd
beargame123 <hoyoung7827@gmail.com>
2022-08-31 23:31:40 +0900

    ➕:: repository 수정

--- OauthTest/src/main/java/com/example/oauthtest/repository/UserRepository.java
+++ OauthTest/src/main/java/com/example/oauthtest/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.example.oauthtest.repository;
 
 import com.example.oauthtest.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
