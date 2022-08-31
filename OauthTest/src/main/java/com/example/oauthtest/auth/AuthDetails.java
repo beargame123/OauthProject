593ad87a238419a0321adab66b094d83e2bd44b0
beargame123 <hoyoung7827@gmail.com>
2022-08-31 23:32:38 +0900

    ➕:: details 수정

--- OauthTest/src/main/java/com/example/oauthtest/auth/AuthDetails.java
+++ OauthTest/src/main/java/com/example/oauthtest/auth/AuthDetails.java
@@ -1,48 +1,63 @@
 package com.example.oauthtest.auth;
 
 import com.example.oauthtest.entity.User;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @RequiredArgsConstructor
+@Getter
 public class AuthDetails implements UserDetails {
-    private User user;
+
+    private final User user;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(user.getRole().name()));
+        return roles;
     }
 
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
-
+    // 사용자의 id를 반환
     @Override
     public String getUsername() {
         return user.getEmail();
     }
 
+    // 사용자의 pw를 반환
+    @Override
+    public String getPassword(){
+        return user.getPassword();
+    }
+
+    // 계정 만료 여부 반환
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true; //만료 안됨
     }
 
+    // 계정 잠금 여부 반환
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true; //잠금 안됨
     }
 
+    // 패스워드의 만료 여부 반환
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true; //만료 안됨
     }
 
+    // 계정 사용 가능 여부 반환
     @Override
     public boolean isEnabled() {
-        return false;
+        return true; //계정 사용 가능
     }
 }
+
