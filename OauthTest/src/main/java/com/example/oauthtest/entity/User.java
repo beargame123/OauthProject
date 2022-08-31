fed0dadf07fbc75b1fb3910dfaedbddc639744c4
beargame123 <hoyoung7827@gmail.com>
2022-08-31 23:31:11 +0900

    🛠️:: user 수정

--- OauthTest/src/main/java/com/example/oauthtest/entity/User.java
+++ OauthTest/src/main/java/com/example/oauthtest/entity/User.java
@@ -1,14 +1,12 @@
 package com.example.oauthtest.entity;
 
+import com.example.oauthtest.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
@@ -27,6 +25,9 @@ public class User {
 
     @Column(name = "introduce")
     private String introduce;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Builder
     public User(String email, String password, String introduce){
