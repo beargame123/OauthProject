f079619e1e37fa32dd11cd87b73e87c061e01b5d
beargame123 <hoyoung7827@gmail.com>
2022-09-09 23:53:19 +0900

    🛠️ filter 완성

--- OauthTest/src/main/java/com/example/oauthtest/filter/JwtAuthenticationFilter.java
+++ OauthTest/src/main/java/com/example/oauthtest/filter/JwtAuthenticationFilter.java
@@ -3,12 +3,15 @@ package com.example.oauthtest.filter;
 import com.example.oauthtest.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @RequiredArgsConstructor
@@ -19,5 +22,19 @@ public class JwtAuthenticationFilter extends GenericFilterBean{
     // request 로 들어가는 Jwt의 유효성 검증
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // request 에서 token 취함.
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest)request);
+
+        // 검증
+        log.info("토큰을 확인하는중");
+        log.info(((HttpServletRequest) request).getRequestURI());
+
+        if(token != null && jwtTokenProvider.validateToken(token)){
+            // 토큰이 유효하면 토큰으로부터 유저 정보를 가져옴
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            // SpringContext 에 Authentication 객체를 가져옴
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request,response);
     }
 }
