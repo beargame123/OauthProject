892c036fed9dac9d9a1854331d55098e70503375
beargame123 <hoyoung7827@gmail.com>
2022-09-13 19:42:35 +0900

    ➕:: 계정 삭제 로직 추가

--- OauthTest/src/main/java/com/example/oauthtest/service/UserService.java
+++ OauthTest/src/main/java/com/example/oauthtest/service/UserService.java
@@ -1,8 +1,10 @@
 package com.example.oauthtest.service;
 
 import com.example.oauthtest.dto.request.UserRequest;
+import com.example.oauthtest.entity.User;
 import com.example.oauthtest.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +14,15 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserRequest myPage(String email){
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new BadCredentialsException("NOT_FOUND_USER"));
+        return UserRequest.builder()
+                .email(user.getEmail())
+                .introduce(user.getIntroduce()).build();
+    }
 
+    public String leave(String email){
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new BadCredentialsException("NOT_FOUND_USER"));
+        userRepository.delete(user);
+        return "계정이 삭제되었습니다.";
     }
 }
