7502efae8bb30304b1806a250756be1609c6c963
beargame123 <hoyoung7827@gmail.com>
2022-09-14 14:14:24 +0900

    🛠️:: Login 로직 LoginController로 이동

--- OauthTest/src/main/java/com/example/oauthtest/controller/UserController.java
+++ OauthTest/src/main/java/com/example/oauthtest/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.oauthtest.entity.User;
 import com.example.oauthtest.enums.Role;
 import com.example.oauthtest.jwt.JwtTokenProvider;
 import com.example.oauthtest.repository.UserRepository;
+import com.example.oauthtest.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -23,46 +24,28 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 @RequiredArgsConstructor
 public class UserController {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
-
-    @PostMapping("/signup")
-    public void signUp(@RequestBody UserRequest request){
-        userRepository.save(User.builder()
-                .email(request.email)
-                .introduce(request.introduce)
-                .password(passwordEncoder.encode(request.password))
-                .role(Role.ROLE_USER)
-                .build());
-    }
-
-    @GetMapping(value = "/logout")
-    public String logoutPage(HttpServletRequest request, HttpServletResponse response){
-        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
-        return "redirect/logout";
-    }
-
-    @PostMapping("/login")
-    public TokenDto login(@RequestBody UserRequest request){
-        User user = userRepository.findByEmail(request.getEmail()).orElseThrow(() -> new IllegalArgumentException("YOUR_NOT_SIGNUP"));
-        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())){
-            log.error("WRONG_PASSWORD");
-        }
-        return jwtTokenProvider.createToken(user.getEmail(), user.getRole());
-    }
-
-    @DeleteMapping("/leave/{id}")
-    public void leaveUser(@PathVariable Long id){
-        User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("USER_NOT_FOUND"));
-        userRepository.delete(user);
-    }
+    private final UserService userService;
 
     @GetMapping("/my")
-    public void myPage(Authentication authentication){
+    public UserRequest myPage(Authentication authentication){
         if(authentication == null){
             throw new BadCredentialsException("USER_INFO_NOT_FOUND");
         }
-        return user
+        return userService.myPage(authentication.getName());
+    }
+
+    @GetMapping("/user/page")
+    public String userPage(){
+        return "유저 페이지에 진입을 성공하였습니다!";
+    }
+
+    @GetMapping("/admin/page")
+    public String adminPage(){
+        return "유저 페이지에 진입을 성공하였습니다!";
+    }
+
+    @GetMapping("/all/page")
+    public String allPage() {
+        return "권한이 없는 페이지에 진입을 성공하였습니다!";
     }
 }
