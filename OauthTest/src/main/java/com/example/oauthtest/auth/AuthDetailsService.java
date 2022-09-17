4d0bd4f3049167299ba572511c787b6e6b7eac96
beargame123 <hoyoung7827@gmail.com>
2022-09-17 23:57:39 +0900

    🛠️:: Oauth 기능 구현중..

--- OauthTest/src/main/java/com/example/oauthtest/auth/AuthDetailsService.java
+++ OauthTest/src/main/java/com/example/oauthtest/auth/AuthDetailsService.java
@@ -5,11 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthDetailsService implements UserDetailsService {
+public class AuthDetailsService implements UserDetailsService, OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final UserRepository userRepository;
 
@@ -18,5 +23,17 @@ public class AuthDetailsService implements UserDetailsService {
         return  userRepository.findByEmail(email)
                 .map(AuthDetails::new)
                 .orElseThrow(() -> new UsernameNotFoundException("NOT_FOUND_USER"));
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        
     }
 }
