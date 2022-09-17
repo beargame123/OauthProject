4b0fc2cbf460a7bbd8290c301112872fac197840
beargame123 <hoyoung7827@gmail.com>
2022-09-17 23:56:48 +0900

    🛠️:: 회원가입/로그인 기능 구현

--- OauthTest/src/main/java/com/example/oauthtest/controller/LoginController.java
+++ OauthTest/src/main/java/com/example/oauthtest/controller/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
